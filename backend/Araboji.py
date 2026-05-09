85adc3072311f2b35ea9c1d82e7de36e4174f2c7
Jabal129 <marwanmazen2001@gmail.com>
2026-05-09 22:50:54 +0200

    showing Arabic only in Araboji

--- backend/Araboji.py
+++ backend/Araboji.py
@@ -144,4 +144,4 @@ def arabize_romaji(romaji_text, mapping):
 def japanese_to_arabic(text, mapping):
     romaji = japanese_to_romaji(text)
     arabic = arabize_romaji(romaji, mapping)
-    return romaji, arabic
+    return arabic
